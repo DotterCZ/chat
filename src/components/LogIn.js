0e23703ff95ca135ac185f8a2f769c43868d5103
DotterCZ <dotter.cz@gmail.com>
2021-10-19 13:02:00 +0200

    login-page draft final test

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -24,13 +24,46 @@ const LogIn = (props) => {
     }
 
     return (
+
+
+
+
+
+
+
+
+        
         <div className="login-wrapper">
             <div className='login-inner'>
-                <input required type="text" name="nickname" value={nickname} onChange={e => setNickname(e.target.value)} placeholder="Přezdívka" className="login-input" />
-                <input type="email" name="login" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" className="login-input" />
-                <Link to={link} className="login-btn">Odeslat</Link>
+                <h1>Online trénink zdarma</h1>
+                
+                <div className='login-inner-flex'>
+                    <div className='input-div'>
+                        <input required type="text" name="nickname" value={nickname} onChange={e => setNickname(e.target.value)} placeholder="Přezdívka" className="login-input" />    
+                        <input type="email" name="login" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" className="login-input" />
+                        <div className='input-div link'>
+                            <Link to={link} className="login-btn"><img src='https://webinar.frantisekklima.cz/images/login-icon.png' alt='login-icon'/><span>Přihlásit se</span></Link>
+                        </div>
+                    </div>
+                    <div className='p-div'>
+                        <p>*Na tento email tě budeme kontaktovat zpětně, abychom odpovědeli na tvé dotazy. Email je dobrovolný.</p>    
+                        <p>*Přezdívka bude viditelná ostatním u každého komentáře, který napíšeš.</p>
+                    </div>
+                </div>
+
+
+                
             </div>
         </div>
+
+
+
+
+
+
+
+
+
     )
 }
 
